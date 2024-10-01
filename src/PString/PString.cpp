2a5f0974c1e98f6818c8fbce5968a186932ab1ae
WatcherBox <watcherbox@csem.ch>
2024-10-01 17:22:07 +0200

    fix files

--- src/PString/PString.cpp
+++ src/PString/PString.cpp
@@ -1,5 +1,5 @@
 /*
-  PString.h - Lightweight printable string class
+  PString.cpp - Lightweight printable string class
   Copyright (c) 2009-2012 Mikal Hart.  All right reserved.
   Copyright (c) 2020 Abhijit Bose (@boseji) 
       - v3.0.1 - Modifications to advance Hex printing functions
@@ -19,84 +19,62 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef PString_h
-#define PString_h
+#include "PString.h"
 
-#include "Print.h"
-#include <stdarg.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <string.h>
-
-#define PSTRING_LIBRARY_VERSION 3
-
-class PString : public Print
+void PString::begin()
 {
-private:
-  char *_buf, *_cur;
-  size_t _size;
-public:
+  _cur = _buf;
+  if (_size > 0)
+    _buf[0] = '\0';
+}
+
 #if defined(ARDUINO) && ARDUINO >= 100
-	virtual size_t write(uint8_t);
+size_t PString::write(uint8_t b)
 #else
-	virtual void write(uint8_t);
+void PString::write(uint8_t b)
 #endif
-
-public:
-
-  // Basic constructor requires a preallocated buffer
-  PString(char *buf, size_t size) : _buf(buf), _size(size)
-  { begin(); }
-
-  // templated constructors allow inline renderings of this type: 
-  //    PString(buf, size, myfloat[, modifier]);
-  template<class T> PString(char *buf, size_t size, T arg) : _buf(buf), _size(size) 
-  { begin(); print(arg); }
-  
-  template<class T> PString(char *buf, size_t size, T arg, int modifier) : _buf(buf), _size(size) 
-  { begin(); print(arg, modifier); }
-
-  // returns the length of the current string, not counting the 0 terminator
-  inline const size_t length() 
-  { return _cur - _buf; }
-
-  // returns the capacity of the string
-  inline const size_t capacity() 
-  { return _size; }
-
-  // gives access to the internal string
-  inline operator const char *() 
-  { return _buf; }
-
-  // compare to another string
-  bool operator==(const char *str) 
-  { return _size > 0 && !strcmp(_buf, str); }
-
-  // call this to re-use an existing string
-  void begin();
-
-  // This function allows assignment to an arbitrary scalar value like 
-  //    str = myfloat;
-  template<class T> inline PString &operator =(T arg) 
-  { begin(); print(arg); return *this; }
-
-  // Concatenation of any type data
-  //    str += myfloat;
-  template<class T> inline PString &operator +=(T arg) 
-  { print(arg); return *this; }
-
-  // Safe access to sprintf-like formatting, 
-  // e.g. str.format("Hi, my name is %s and I'm %d years old", name, age);
-  int format(char *str, ...);
-
-  // Print Hex Byte to the String
-  void printHexByte(uint8_t data);
-
-  // Print a Hex Buffer to the String
-  void printHexBuffer(char* buf, size_t size);
-
-  // Print a Hex Buffer like C Array to the String
-  void printHexBufferArr(char* buf, size_t size);
-};
-
+{
+  if (_cur + 1 < _buf + _size)
+  {
+    *_cur++ = (char)b;
+    *_cur = '\0';
+#if defined(ARDUINO) && ARDUINO >= 100
+		return 1;
 #endif
+	}
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	return 0;
+#endif
+}
+
+int PString::format(char *str, ...) 
+{ 
+  va_list argptr;  
+  va_start(argptr, str); 
+  int ret = vsnprintf(_cur, _size - (_cur - _buf), str, argptr);
+  if (_size)
+     while (*_cur) 
+        ++_cur;
+  return ret;
+}
+
+void PString::printHexByte(uint8_t data) {
+    static const char map[]={"0123456789ABCDEF"};
+    print(map[(uint8_t)((data&0xF0)>>4)]);
+    print(map[(uint8_t)(data&0x0F)]);
+}
+
+void PString::printHexBuffer(char* buf, size_t size) {
+  for(size_t i = 0; i < size ; i++) {
+    printHexByte((uint8_t)buf[i]);
+  }
+}
+
+void PString::printHexBufferArr(char* buf, size_t size) {
+  for(size_t i = 0; i < size ; i++) {
+    print(F(" 0x"));
+    printHexByte((uint8_t)buf[i]);
+    print(',');
+  }
+}
